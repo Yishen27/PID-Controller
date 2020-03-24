9ab964bec2fb332ac28db4941ff5d25377a4e5be
Yishen27 <55043085+Yishen27@users.noreply.github.com>
2020-03-24 19:21:16 +0800

    Add files via upload

---
+++ PID/home/CarND-PID-Control-Project/src/PID.h
@@ -0,0 +1,59 @@
+#ifndef PID_H
+#define PID_H
+#include <vector>
+
+using std::vector;
+
+class PID {
+ public:
+  /**
+   * Constructor
+   */
+  PID();
+
+  /**
+   * Destructor.
+   */
+  virtual ~PID();
+
+  /**
+   * Initialize PID.
+   * @param (Kp_, Ki_, Kd_) The initial PID coefficients
+   */
+  void Init(double Kp_, double Ki_, double Kd_);
+  
+  vector<double> update_k(vector<double> p);
+
+  /**
+   * Update the PID error variables given cross track error.
+   * @param cte The current cross track error
+   */
+  void UpdateError(double cte);
+
+  /**
+   * Calculate the total PID error.
+   * @output The total PID error
+   */
+  double TotalError();
+  
+  double twiddle(double tol, double cte);
+  
+  
+
+ private:
+  /**
+   * PID Errors
+   */
+  double p_error;
+  double i_error;
+  double d_error;
+
+  /**
+   * PID Coefficients
+   */ 
+  double Kp;
+  double Ki;
+  double Kd;
+};
+
+#endif  // PID_H
